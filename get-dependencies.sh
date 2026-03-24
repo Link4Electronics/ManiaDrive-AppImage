a50f05cb4b30ddeb2c6a5f9bfe8ff14c2d130cba
Link4Electronics <link4electronics@gmail.com>
2026-03-24 09:51:57 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,14 +6,17 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm \
+    freealut
+    libxml2-legacy
+    libcurl-gnutls
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+make-aur-package libpng12
 
 # If the application needs to be manually built that has to be done down here
 
